c485464d0b576823a054915ee0151f87e9a82e94
torra3167 <torra3167@gmail.com>
2018-11-07 14:02:12 +0900

    1

--- final_project/src/main/java/service/MemberService.java
+++ final_project/src/main/java/service/MemberService.java
@@ -1,10 +1,11 @@
 package service;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
 
 import model.Member;
 import repository.MemberRepository;
-
+@Service
 public class MemberService {
 	@Autowired
 	private MemberRepository mr;
