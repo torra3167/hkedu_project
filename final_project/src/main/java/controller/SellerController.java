e6807b8e2004b527d7fcfaf0db3e0db8cc442c4f
torra3167 <torra3167@gmail.com>
2018-11-07 16:17:37 +0900

    no message

--- final_project/src/main/java/controller/SellerController.java
+++ final_project/src/main/java/controller/SellerController.java
@@ -18,7 +18,7 @@ public class SellerController {
 		private SellerService sellerService;
 		
 		
-		@RequestMapping(value="/seller/join.gom", method=RequestMethod.GET)
+		@RequestMapping(value="/seller_join.gom", method=RequestMethod.GET)
 		public String joinForm(Model model) {
 			model.addAttribute("sellerJoinCommand", new SellerJoinCommand());
 			return "seller/seller_join.jsp";
