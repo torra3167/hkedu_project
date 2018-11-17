1f0d49992ccca6217ee4b70c6eef88ce9bfc21e5
jeongpil jang <torra3167@gmail.com>
2018-11-17 10:55:50 +0900

    no message

--- final_project/src/main/java/service/ProgramService.java
+++ final_project/src/main/java/service/ProgramService.java
@@ -30,8 +30,8 @@ public class ProgramService {
 	ProgramExercise programExercise;
 	MultipartFile multiFile;
 	static final String filePath = 
-			"C:\\Users\\HKEDU\\Documents\\hkedu_project\\final_project\\src\\main\\webapp\\WEB-INF\\resource\\";
-//			"C:\\Users\\admin\\Documents\\final_project\\final_project\\src\\main\\webapp\\WEB-INF\\resource\\";
+//			"C:\\Users\\HKEDU\\Documents\\hkedu_project\\final_project\\src\\main\\webapp\\WEB-INF\\resource\\";
+			"C:\\Users\\Future\\Documents\\hkedu_project\\final_project\\src\\main\\webapp\\WEB-INF\\resource\\";	
 	File file = new File(filePath);
 
 	@Autowired
