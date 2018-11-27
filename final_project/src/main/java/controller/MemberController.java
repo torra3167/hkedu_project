0d8192391d3e713869ec7bdc666aa1345b5372d3
torra3167 <torra3167@gmail.com>
2018-11-27 09:47:28 +0900

    123

--- final_project/src/main/java/controller/MemberController.java
+++ final_project/src/main/java/controller/MemberController.java
@@ -45,6 +45,7 @@ public class MemberController {
 		}
 	}
 	
+	
 	@RequestMapping(value="/findID.gom", method=RequestMethod.GET)
 	public String findID(FindIDCommand findIDCommand,Model model) {
 		model.addAttribute("iPage", "login/findID.jsp");
