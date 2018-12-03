a26a9625236eac21d51febf1735a27f1cfd4e674
DahaeK <43315201+DahaeK@users.noreply.github.com>
2018-12-03 13:37:43 +0900

    no message

--- final_project/src/main/java/command/MemberSurveyCommand.java
+++ final_project/src/main/java/command/MemberSurveyCommand.java
@@ -59,91 +59,76 @@ public class MemberSurveyCommand implements Serializable {
 		this.survCareer = survCareer;
 	}
 
-
-
 	public int getSurvNo() {
 		return survNo;
 	}
+
 	public void setSurvNo(int survNo) {
 		this.survNo = survNo;
 	}
+
 	public String getMemberEmail() {
 		return memberEmail;
 	}
+
 	public void setMemberEmail(String memberEmail) {
 		this.memberEmail = memberEmail;
 	}
+
 	public int getSurvHeight() {
 		return survHeight;
 	}
+
 	public void setSurvHeight(int survHeight) {
 		this.survHeight = survHeight;
 	}
+
 	public int getSurvWeight() {
 		return survWeight;
 	}
+
 	public void setSurvWeight(int survWeight) {
 		this.survWeight = survWeight;
 	}
+
 	public String getSurvEat() {
 		return survEat;
 	}
+
 	public void setSurvEat(String survEat) {
 		this.survEat = survEat;
 	}
+
 	public int getSurvBMI() {
 		return survBMI;
 	}
+
 	public void setSurvBMI(int survBMI) {
 		this.survBMI = survBMI;
 	}
+
 	public String getSurvSex() {
 		return survSex;
 	}
+
 	public void setSurvSex(String survSex) {
 		this.survSex = survSex;
 	}
+
 	public int getSurvAge() {
 		return survAge;
 	}
+
 	public void setSurvAge(int survAge) {
 		this.survAge = survAge;
 	}
+
 	public String getSurvLifestyle() {
 		return survLifestyle;
 	}
+
 	public void setSurvLifestyle(String survLifestyle) {
 		this.survLifestyle = survLifestyle;
-	}
-	public String getSurvShape() {
-		return survShape;
-	}
-	public void setSurvShape(String survShape) {
-		this.survShape = survShape;
-	}
-	public String getSurvDisease() {
-		return survDisease;
-	}
-	public void setSurvDisease(String survDisease) {
-		this.survDisease = survDisease;
-	}
-	public String getSurvConcern() {
-		return survConcern;
-	}
-	public void setSurvConcern(String survConcern) {
-		this.survConcern = survConcern;
-	}
-	public String getSurvCause() {
-		return survCause;
-	}
-	public void setSurvCause(String survCause) {
-		this.survCause = survCause;
-	}
-	public String getSurvCareer() {
-		return survCareer;
-	}
-	public void setSurvCareer(String survCareer) {
-		this.survCareer = survCareer;
 	}
 
 	public String getSurveyDietPeriod() {
@@ -153,4 +138,46 @@ public class MemberSurveyCommand implements Serializable {
 	public void setSurveyDietPeriod(String surveyDietPeriod) {
 		this.surveyDietPeriod = surveyDietPeriod;
 	}
+
+	public String getSurvShape() {
+		return survShape;
+	}
+
+	public void setSurvShape(String survShape) {
+		this.survShape = survShape;
+	}
+
+	public String getSurvDisease() {
+		return survDisease;
+	}
+
+	public void setSurvDisease(String survDisease) {
+		this.survDisease = survDisease;
+	}
+
+	public String getSurvConcern() {
+		return survConcern;
+	}
+
+	public void setSurvConcern(String survConcern) {
+		this.survConcern = survConcern;
+	}
+
+	public String getSurvCause() {
+		return survCause;
+	}
+
+	public void setSurvCause(String survCause) {
+		this.survCause = survCause;
+	}
+
+	public String getSurvCareer() {
+		return survCareer;
+	}
+
+	public void setSurvCareer(String survCareer) {
+		this.survCareer = survCareer;
+	}
+
+
 }
